60b37c7a726a401d5da6d4c10d542576f7124d8d
Jon Kent <jonkent95@gmail.com>
2021-04-21 15:53:54 +0100

    Server class

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY . /app
 WORKDIR /app
 RUN pip install --no-cache-dir -U -r /app/requirements.txt
 EXPOSE 5000
-CMD ["gunicorn" , "--bind=0.0.0.0:5000", "--workers=2", "run:app"]
+# CMD ["gunicorn" , "--bind=0.0.0.0:5000", "--workers=2", "run:app"]
+CMD ["python", "./run.py"]
--- run.py
+++ run.py
@@ -1,9 +1,36 @@
+from gunicorn.app.base import BaseApplication
+
 from app import app, cloud_config
 import structlog
 
 logger = structlog.get_logger()
 
+
+class Server(BaseApplication):
+
+    def __init__(self, application, options=None):
+        self.options = options or {}
+        self.application = application
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items()
+                  if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+        cloud_config()
+
+    def load(self):
+        return self.application
+
+
 if __name__ == '__main__':
     logger.info('Starting SDX Deliver')
-    cloud_config()
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    options = {
+        'bind': '%s:%s' % ('0.0.0.0', '5000'),
+        'workers': 2,
+    }
+    Server(app, options).run()
+
+    # app.run(debug=False, host='0.0.0.0', port=5000)
