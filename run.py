cb85aa9e3fc96a3f3d42ba00382aae9f9ad9c010
TomHolroyd <tomholroyd96@gmail.com>
2021-05-19 15:54:31 +0100

    Testing

--- run.py
+++ run.py
@@ -1,11 +1,36 @@
 import structlog
 import uvicorn
 
-from app import app
+from app import app, cloud_config
 
 logger = structlog.get_logger()
 
 
+# class Server(BaseApplication):
+#
+#     def __init__(self, application, options_dict=None):
+#         self.options = options_dict or {}
+#         self.application = application
+#         super().__init__()
+#
+#     def load_config(self):
+#         config = {key: value for key, value in self.options.items()
+#                   if key in self.cfg.settings and value is not None}
+#         for key, value in config.items():
+#             self.cfg.set(key.lower(), value)
+#
+#         cloud_config()
+#
+#     def load(self):
+#         return self.application
+
+
 if __name__ == '__main__':
     logger.info('Starting SDX Deliver')
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    cloud_config()
+    uvicorn.run(app, host="0.0.0.0", port=5003)
+    # options = {
+    #     'bind': '%s:%s' % ('0.0.0.0', '5003'),
+    #     'workers': 2,
+    # }
+    # Server(app, options).run()
