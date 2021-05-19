1a969f431eef8b6f92c2fd02050373ce115eff6f
TomHolroyd <tomholroyd96@gmail.com>
2021-05-19 15:49:05 +0100

    Removing base appliacation

--- run.py
+++ run.py
@@ -1,29 +1,9 @@
 import structlog
 import uvicorn
-from gunicorn.app.base import BaseApplication
 
-from app import app, cloud_config
+from app import app
 
 logger = structlog.get_logger()
-
-
-class Server(BaseApplication):
-
-    def __init__(self, application, options_dict=None):
-        self.options = options_dict or {}
-        self.application = application
-        super().__init__()
-
-    def load_config(self):
-        config = {key: value for key, value in self.options.items()
-                  if key in self.cfg.settings and value is not None}
-        for key, value in config.items():
-            self.cfg.set(key.lower(), value)
-
-        cloud_config()
-
-    def load(self):
-        return self.application
 
 
 if __name__ == '__main__':
